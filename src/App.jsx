1f5b27dd4399d964574385e9b259980608fac469
DanielC307 <daniel.caetano@univille.br>
2026-04-06 14:13:31 -0300

    listando eventos

--- src/App.jsx
+++ src/App.jsx
@@ -46,18 +46,27 @@ function App() {
     }
   ]
 
+  function adicionarEvento(evento){
+    eventos.push(evento)
+    console.log('eventos =>', eventos)
+  }
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
       <Banner />
-      <FormularioDeEventos temas={temas}/>
+      <FormularioDeEventos temas={temas} aoSubmeter={adicionarEvento} />
       {temas.map(function (item) {
         return (
           <section key={item.id}>
             <Tema tema={item} />
-            <CardEvento evento={eventos[0]} />
+            {eventos.map(function (item, index) {
+
+              return <CardEvento evento={item} key={index} />
+            })}
+            
           </section>
         )
       })}
--- src/componentes/FormularioDeEventos/index.jsx
+++ src/componentes/FormularioDeEventos/index.jsx
@@ -6,7 +6,7 @@ import { CampoDeEntrada } from '../CampoDeEntrada'
 import { Botao } from '../botao'
 import { ListaSuspensa } from '../ListaSuspensa'
 
-export function FormularioDeEventos({ temas }) {
+export function FormularioDeEventos({ temas, aoSubmeter }) {
 
   function aoFormSubmetido(formData) {
     console.log('opa, tá na hora de criar um novo evento', formData);
@@ -18,7 +18,7 @@ export function FormularioDeEventos({ temas }) {
       data: new Date(formData.get('dataEvento')),
       titulo: formData.get('nomeEvento')
     }
-    console.log('Esse é o evento', evento);
+    aoSubmeter(evento);
 
   }
 
